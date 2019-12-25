0552f3e83e96750af04c3bfc89c9828f9930682e
wjn <w895350975@gmail.com>
2019-12-24 21:47:52 -0500

    add system call

--- kernel/fcntl.h
+++ kernel/fcntl.h
@@ -2,3 +2,11 @@
 #define O_WRONLY  0x001
 #define O_RDWR    0x002
 #define O_CREATE  0x200
+
+// same with PTE_ in riscv.h, so we can directly
+// use the VMA's prot as PTE's permission in trap.c
+#define PROT_READ   PTE_R
+#define PROT_WRITE  PTE_W
+
+#define MAP_SHARED  0x001
+#define MAP_PRIVATE 0x002
